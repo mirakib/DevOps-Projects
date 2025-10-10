4a1846d828ba6e9799ede5aef66574b7881bf7bb
MI RAKIB <arakibkhan003@gmail.com>
2025-10-11 01:24:25 +0600

    Update dashboard titles in Django and Flask apps
    Changed the HTML <title> in the Django app to 'Django Dashboard' and in the Flask app to 'Flask Dashboard' for clearer identification of each application's dashboard.

--- Docker/Flask Static Frontend Application/Multi-Stage-Build/app.py
+++ Docker/Flask Static Frontend Application/Multi-Stage-Build/app.py
@@ -9,7 +9,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Docker & Flask Dashboard</title>
+    <title>Flask Dashboard</title>
     <!-- Google Font: Poppins -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap" rel="stylesheet">
     <!-- Font Awesome for Icons (using a common CDN) -->
