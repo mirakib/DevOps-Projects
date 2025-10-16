8b90a84d79b6f7516179cb8c0adf1d5300f6f628
MOSHREKUL ISLAM <45521827+mirakib@users.noreply.github.com>
2025-10-17 00:05:21 +0600

    Delete Stacks/Only Docker Based/LAMP CRUD Application directory

--- Stacks/Only Docker Based/LAMP CRUD Application/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("db", "user", "userpass", "myapp");
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-
-echo "Connected to MySQL successfully!";
-?>
