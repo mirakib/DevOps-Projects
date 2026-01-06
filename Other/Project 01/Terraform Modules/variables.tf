b485596a34c2f4c0afa3e9ada6d793a32d1a2f31
MOSHREKUL ISLAM <45521827+mirakib@users.noreply.github.com>
2026-01-06 23:51:57 +0600

    Add Terraform variables for region and networking

---
+++ Other/Project 01/Terraform Modules/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Existing EC2 key pair"
+}
