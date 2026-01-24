50e7c932acc0d5faea03feae00de34d8be4f32bc
Moshrekul Islam <arakibkhan003@gmail.com>
2026-01-25 02:42:17 +0600

    Update AWS region to us-east-1 in Terraform config
    Changed the AWS region from ap-south-1 to us-east-1 in provider and subnet resources. Updated .gitignore to exclude .terraform.lock.hcl.

--- Other/Project 02/Terraform Modules/main.tf
+++ Other/Project 02/Terraform Modules/main.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   tags                    = { Name = "subnet-public" }
 }
 
