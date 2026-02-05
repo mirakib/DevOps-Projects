8156348ece5385ce6e0daf9f8bc81ff07bd86782
Moshrekul Islam <arakibkhan003@gmail.com>
2026-02-05 23:19:34 +0600

    Remove Other/Project 04 and Project 05 files
    Remove obsolete demo projects under Other/: delete Project 04 assets (README and ecs-workshop-personal.yaml CloudFormation template) and Project 05 artifacts (GitHub Actions workflow, Dockerfile, README, and Terraform files including Node.js-environment.tfvars, main.tf, outputs.tf, variables.tf). This cleans up unused/example infrastructure and app deployment resources.

--- Other/Project 05/terraform/Node.js-environment.tfvars
+++
@@ -1,3 +0,0 @@
-aws_region      = "us-east-1a" 
-key_name        = "generated-key" 
-private_key_path = "generated-key.pem"
--- Other/Project 05/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_ip" { 
-  description = "Public IP of the instance" 
-  value       = aws_instance.react_native_notes.public_ip 
-}
